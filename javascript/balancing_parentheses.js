ef918efc22c96eed615c17728b43c1b4f2add451
+ <+>
2022-06-21 12:56:09 -0400

    solution

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,21 +1,34 @@
 function balancingParentheses(string) {
-  // type your code here
+  let opening = 0;
+  let missing = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "(") {
+      opening += 1;
+      continue;
+    }
+    if (opening > 0) {
+      --opening;
+    } else {
+      ++missing;
+    }
+  }
+  return missing + opening;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 0");
-  console.log(balancingParentheses('(()())'));
+  console.log(balancingParentheses("(()())"));
 
   console.log("");
 
   console.log("Expecting: 2");
-  console.log(balancingParentheses('()))'));
+  console.log(balancingParentheses("()))"));
 
   console.log("");
 
   console.log("Expecting: 1");
-  console.log(balancingParentheses(')'));
+  console.log(balancingParentheses(")"));
 }
 
 module.exports = balancingParentheses;
